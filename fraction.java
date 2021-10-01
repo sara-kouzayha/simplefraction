db1429da171032adae002e77092662670703455b
kassemchakaroun <kassem.chakaroun@outlook.com>
2021-10-01 13:03:34 +0300

    main.java

--- fraction.java
+++ fraction.java
@@ -1,4 +1,29 @@
-public class fraction {
-    
+public class fraction 
+{
+    public  int x;
+    public   int y;
+
+    public  fraction(int x,int y)
+    {
+        this.x=x;
+        if(y==0)
+        {
+            System.out.println("y is zero");
+        }else this.y=y;
+    }
+    /*public static int getx()
+    {
+        return x;
+    }
+    public static int gety()
+    {
+        return y;
+    }*/
+    public static void tostring1(int x,int y)
+    {
+        System.out.println(x+"/"+y);
+    }
+      
+
 }
 
--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        fraction c1 = new fraction(1,0);
+        c1.tostring1(1,2);
     }
+    
+    
 }
